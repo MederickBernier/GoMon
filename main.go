979f50b49a62c82f333387e84c8598a45254286b
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-01-08 12:48:01 -0500

    added /system/cpu endpoint for monitoring cpu usage

--- main.go
+++ main.go
@@ -5,12 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/shirou/gopsutil/cpu"
 )
 
 func main(){
 	// Define a basic route
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request){
 		response := map[string]string{"Status":"OK"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	// Define a route to get CPU usage
+	http.HandleFunc("/system/cpu", func(w http.ResponseWriter, r *http.Request){
+		// get the CPU usage
+		usage, err := cpu.Percent(0,false)
+		if err != nil{
+			http.Error(w, "Error getting CPU usage", http.StatusInternalServerError)
+			return
+		}
+
+		// return the json response
+		response := map[string]float64{"cpu_usage": usage[0]}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
 	})
