746308ea5c11f2e2029766030f554f40de2f7d54
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-01-08 13:03:30 -0500

    Added /system/memory endpoint for monitoring memory usage

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 func main(){
@@ -28,6 +29,24 @@ func main(){
 
 		// return the json response
 		response := map[string]float64{"cpu_usage": usage[0]}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	// Define a route to get Memory usage
+	http.HandleFunc("/system/memory", func(w http.ResponseWriter, r *http.Request){
+		memoryStats, err := mem.VirtualMemory()
+		if err != nil{
+			http.Error(w,"Error getting Memory usage", http.StatusInternalServerError)
+			return
+		}
+
+		response := map[string]interface{}{
+			"total_memory":memoryStats.Total,
+			"used_memory":memoryStats.Used,
+			"free_memory":memoryStats.Free,
+			"used_percent":memoryStats.UsedPercent,
+		}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
 	})
