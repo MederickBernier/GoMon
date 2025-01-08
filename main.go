2322e2f4aee15bde41bc19e8aff08f0fef157fa9
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-01-08 18:04:08 -0500

    moved the cpu logic to the package instead of having it direcly in the main.go file

--- main.go
+++ main.go
@@ -1,58 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/MederickBernier/GoMon/monitor"
 	"github.com/MederickBernier/GoMon/utils"
-	"github.com/shirou/gopsutil/v3/cpu"
-	"github.com/shirou/gopsutil/v3/mem"
 )
 
 func main() {
-	// Define a basic route
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		response := map[string]string{"Status": "OK"}
-		utils.LogRequest(r)
-		utils.JSONResponse(w, response, http.StatusOK)
-	})
-
-	// Define a route to get CPU usage
 	http.HandleFunc("/system/cpu", func(w http.ResponseWriter, r *http.Request) {
-		// get the CPU usage
-		usage, err := cpu.Percent(0, false)
+		utils.LogRequest(r)
+		data, err := monitor.GetCPUUsage()
 		if err != nil {
-			http.Error(w, "Error getting CPU usage", http.StatusInternalServerError)
+			utils.HandleError(w, err, "Error getting CPU usage", http.StatusInternalServerError)
 			return
 		}
-
-		// return the json response
-		response := map[string]float64{"cpu_usage": usage[0]}
-		utils.JSONResponse(w, response, http.StatusOK)
+		utils.JSONResponse(w, data, http.StatusOK)
 	})
 
-	// Define a route to get Memory usage
-	http.HandleFunc("/system/memory", func(w http.ResponseWriter, r *http.Request) {
-		memoryStats, err := mem.VirtualMemory()
+	http.HandleFunc("/system/cpu/cores", func(w http.ResponseWriter, r *http.Request) {
+		utils.LogRequest(r)
+		data, err := monitor.GetCPUUsagePerCore()
 		if err != nil {
-			http.Error(w, "Error getting Memory usage", http.StatusInternalServerError)
+			utils.HandleError(w, err, "Error getting CPU usage per core", http.StatusInternalServerError)
 			return
 		}
-
-		response := map[string]interface{}{
-			"total_memory": memoryStats.Total,
-			"used_memory":  memoryStats.Used,
-			"free_memory":  memoryStats.Free,
-			"used_percent": memoryStats.UsedPercent,
-		}
-		utils.JSONResponse(w, response, http.StatusOK)
+		utils.JSONResponse(w, map[string][]float64{"cores": data}, http.StatusOK)
 	})
 
-	// Start the server
 	port := "8080"
-	fmt.Printf("Server is running at http://localhost:%s\n", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("Error starting server: %v\n", err)
-	}
+	log.Printf("Server is running at http://localhost:%s\n", port)
+	http.ListenAndServe(":"+port, nil)
 }
--- monitor/cpu.go
+++ monitor/cpu.go
@@ -1,2 +1,23 @@
 package monitor
 
+import (
+	"time"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+)
+
+func GetCPUUsage() (map[string]float64, error) {
+	usage, err := cpu.Percent(500*time.Millisecond, false)
+	if err != nil {
+		return nil, err
+	}
+	return map[string]float64{"cpu_usage": usage[0]}, nil
+}
+
+func GetCPUUsagePerCore() ([]float64, error) {
+	usage, err := cpu.Percent(500*time.Millisecond, true)
+	if err != nil {
+		return nil, err
+	}
+	return usage, nil
+}
