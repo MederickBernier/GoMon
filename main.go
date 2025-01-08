c070673f564e666e24c9e46373fbe212a45bb6a5
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-01-08 10:23:26 -0500

    added basic http server functionalities

--- main.go
+++ main.go
@@ -1,2 +1,24 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	// Define a basic route
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request){
+		response := map[string]string{"Status":"OK"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	// Start the server
+	port := "8080"
+	fmt.Printf("Server is running at http://localhost:%s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Error starting server: %v\n", err)
+	}
+}
